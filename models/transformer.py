0dd210900d8428f7cef6853c43999b841d6c1de3
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-06-02 22:32:59 -0700

    Checkpoint

--- models/transformer.py
+++ models/transformer.py
@@ -19,6 +19,8 @@ from torch.nn.init import xavier_uniform_
 from torch.nn.init import xavier_normal_
 from torch.nn.parameter import Parameter
 
+import nestedtensor
+
 
 class Transformer(nn.Module):
 
@@ -54,7 +56,7 @@ class Transformer(nn.Module):
         # query_embed is a Tensor
         assert mask is None
         # import pdb; pdb.set_trace()
-        query_embed = query_embed.unsqueeze(1) #.repeat(1, len(src), 1)
+        query_embed = query_embed.unsqueeze(1)  # .repeat(1, len(src), 1)
         srcs = []
         poss = []
         tgts = []
@@ -69,21 +71,26 @@ class Transformer(nn.Module):
 
             tgt = torch.zeros_like(query_embed)
 
-            srcs.append(src_i)
-            poss.append(pos_embed_i)
+            # TODO: MHA batching semantics are annoying
+            # import pdb; pdb.set_trace()
+            srcs.append(src_i.squeeze(1))
+            poss.append(pos_embed_i.squeeze(1))
             tgts.append(tgt)
 
-        hs = []
-        for src_i, pos_i, tgt_i in zip(srcs, poss, tgts):
-            memory = self.encoder(src_i, src_key_padding_mask=mask, pos=pos_i)
-            hs_i = self.decoder(tgt_i, memory, memory_key_padding_mask=mask,
-                              pos=pos_i, query_pos=query_embed)
+        src_nt = nestedtensor.nested_tensor(srcs)
+        pos_nt = nestedtensor.nested_tensor(poss)
+        tgt_nt = nestedtensor.nested_tensor(tgts)
+        memory = self.encoder(src_nt, src_key_padding_mask=mask, pos=pos_nt)
 
+        hs = []
+        for tgt_i, pos_i in zip(tgt_nt.unbind(), pos_nt.unbind()):
+            hs_i = self.decoder(tgt_i, memory, memory_key_padding_mask=mask,
+                                 pos=pos_i, query_pos=query_embed)
             hs_i = hs_i.transpose(1, 2)
             hs.append(hs_i)
         # import pdb; pdb.set_trace()
         # TODO: Could accumulate memory and return as NT
-        #, memory.permute(1, 2, 0).view(len(src), c, h, w)
+        # , memory.permute(1, 2, 0).view(len(src), c, h, w)
         return torch.cat(hs, dim=1), None
 
 
@@ -152,6 +159,7 @@ class TransformerDecoder(nn.Module):
         return output
 
 
+# NT case query, key, value have nested_dim 1 and are of shape (bsz, tgt_len, embed_dim)
 def multi_head_attention_forward(query,                           # type: Tensor
                                  key,                             # type: Tensor
                                  value,                           # type: Tensor
@@ -176,7 +184,10 @@ def multi_head_attention_forward(query,                           # type: Tensor
                                  static_k=None,                   # type: Optional[Tensor]
                                  static_v=None                    # type: Optional[Tensor]
                                  ):
-    tgt_len, bsz, embed_dim = query.size()
+    assert isinstance(query, nestedtensor.NestedTensor)
+    assert isinstance(key, nestedtensor.NestedTensor)
+    assert isinstance(value, nestedtensor.NestedTensor)
+    bsz, tgt_len, embed_dim = query.size()
     assert embed_dim == embed_dim_to_check
     # allow MHA to have different sizes for the feature dimension
     assert key.size(0) == value.size(0) and key.size(1) == value.size(1)
@@ -185,173 +196,55 @@ def multi_head_attention_forward(query,                           # type: Tensor
     assert head_dim * num_heads == embed_dim, "embed_dim must be divisible by num_heads"
     scaling = float(head_dim) ** -0.5
 
-    if not use_separate_proj_weight:
-        if torch.equal(query, key) and torch.equal(key, value):
-            # self-attention
-            q, k, v = F.linear(query, in_proj_weight, in_proj_bias).chunk(3, dim=-1)
+    assert not use_separate_proj_weight
+    # This is inline in_proj function with in_proj_weight and in_proj_bias
+    _b = in_proj_bias
+    _start = 0
+    _end = embed_dim
+    _w = in_proj_weight[_start:_end, :]
+    if _b is not None:
+        _b = _b[_start:_end]
+    q = F.linear(query, _w, _b)
 
-        elif torch.equal(key, value):
-            # encoder-decoder attention
-            # This is inline in_proj function with in_proj_weight and in_proj_bias
-            _b = in_proj_bias
-            _start = 0
-            _end = embed_dim
-            _w = in_proj_weight[_start:_end, :]
-            if _b is not None:
-                _b = _b[_start:_end]
-            q = F.linear(query, _w, _b)
+    # This is inline in_proj function with in_proj_weight and in_proj_bias
+    _b = in_proj_bias
+    _start = embed_dim
+    _end = embed_dim * 2
+    _w = in_proj_weight[_start:_end, :]
+    if _b is not None:
+        _b = _b[_start:_end]
+    k = F.linear(key, _w, _b)
 
-            if key is None:
-                assert value is None
-                k = None
-                v = None
-            else:
-
-                # This is inline in_proj function with in_proj_weight and in_proj_bias
-                _b = in_proj_bias
-                _start = embed_dim
-                _end = None
-                _w = in_proj_weight[_start:, :]
-                if _b is not None:
-                    _b = _b[_start:]
-                k, v = F.linear(key, _w, _b).chunk(2, dim=-1)
-
-        else:
-            # This is inline in_proj function with in_proj_weight and in_proj_bias
-            _b = in_proj_bias
-            _start = 0
-            _end = embed_dim
-            _w = in_proj_weight[_start:_end, :]
-            if _b is not None:
-                _b = _b[_start:_end]
-            q = F.linear(query, _w, _b)
-
-            # This is inline in_proj function with in_proj_weight and in_proj_bias
-            _b = in_proj_bias
-            _start = embed_dim
-            _end = embed_dim * 2
-            _w = in_proj_weight[_start:_end, :]
-            if _b is not None:
-                _b = _b[_start:_end]
-            k = F.linear(key, _w, _b)
-
-            # This is inline in_proj function with in_proj_weight and in_proj_bias
-            _b = in_proj_bias
-            _start = embed_dim * 2
-            _end = None
-            _w = in_proj_weight[_start:, :]
-            if _b is not None:
-                _b = _b[_start:]
-            v = F.linear(value, _w, _b)
-    else:
-        q_proj_weight_non_opt = torch.jit._unwrap_optional(q_proj_weight)
-        len1, len2 = q_proj_weight_non_opt.size()
-        assert len1 == embed_dim and len2 == query.size(-1)
-
-        k_proj_weight_non_opt = torch.jit._unwrap_optional(k_proj_weight)
-        len1, len2 = k_proj_weight_non_opt.size()
-        assert len1 == embed_dim and len2 == key.size(-1)
-
-        v_proj_weight_non_opt = torch.jit._unwrap_optional(v_proj_weight)
-        len1, len2 = v_proj_weight_non_opt.size()
-        assert len1 == embed_dim and len2 == value.size(-1)
-
-        if in_proj_bias is not None:
-            q = F.linear(query, q_proj_weight_non_opt, in_proj_bias[0:embed_dim])
-            k = F.linear(key, k_proj_weight_non_opt, in_proj_bias[embed_dim:(embed_dim * 2)])
-            v = F.linear(value, v_proj_weight_non_opt, in_proj_bias[(embed_dim * 2):])
-        else:
-            q = F.linear(query, q_proj_weight_non_opt, in_proj_bias)
-            k = F.linear(key, k_proj_weight_non_opt, in_proj_bias)
-            v = F.linear(value, v_proj_weight_non_opt, in_proj_bias)
+    # This is inline in_proj function with in_proj_weight and in_proj_bias
+    _b = in_proj_bias
+    _start = embed_dim * 2
+    _end = None
+    _w = in_proj_weight[_start:, :]
+    if _b is not None:
+        _b = _b[_start:]
+    v = F.linear(value, _w, _b)
     q = q * scaling
 
-    if attn_mask is not None:
-        assert attn_mask.dtype == torch.float32 or attn_mask.dtype == torch.float64 or \
-            attn_mask.dtype == torch.float16 or attn_mask.dtype == torch.uint8 or attn_mask.dtype == torch.bool, \
-            'Only float, byte, and bool types are supported for attn_mask, not {}'.format(attn_mask.dtype)
-        if attn_mask.dtype == torch.uint8:
-            warnings.warn("Byte tensor for attn_mask in nn.MultiheadAttention is deprecated. Use bool tensor instead.")
-            attn_mask = attn_mask.to(torch.bool)
+    assert attn_mask is None
+    assert key_padding_mask is None
+    assert bias_k is None
+    assert bias_v is None
+    assert static_k is None
+    assert static_v is None
+    assert not add_zero_attn
 
-        if attn_mask.dim() == 2:
-            attn_mask = attn_mask.unsqueeze(0)
-            if list(attn_mask.size()) != [1, query.size(0), key.size(0)]:
-                raise RuntimeError('The size of the 2D attn_mask is not correct.')
-        elif attn_mask.dim() == 3:
-            if list(attn_mask.size()) != [bsz * num_heads, query.size(0), key.size(0)]:
-                raise RuntimeError('The size of the 3D attn_mask is not correct.')
-        else:
-            raise RuntimeError("attn_mask's dimension {} is not supported".format(attn_mask.dim()))
-        # attn_mask's dim is 3 now.
-
-    # convert ByteTensor key_padding_mask to bool
-    if key_padding_mask is not None and key_padding_mask.dtype == torch.uint8:
-        warnings.warn("Byte tensor for key_padding_mask in nn.MultiheadAttention is deprecated. Use bool tensor instead.")
-        key_padding_mask = key_padding_mask.to(torch.bool)
-
-    if bias_k is not None and bias_v is not None:
-        if static_k is None and static_v is None:
-            k = torch.cat([k, bias_k.repeat(1, bsz, 1)])
-            v = torch.cat([v, bias_v.repeat(1, bsz, 1)])
-            if attn_mask is not None:
-                attn_mask = pad(attn_mask, (0, 1))
-            if key_padding_mask is not None:
-                key_padding_mask = pad(key_padding_mask, (0, 1))
-        else:
-            assert static_k is None, "bias cannot be added to static key."
-            assert static_v is None, "bias cannot be added to static value."
-    else:
-        assert bias_k is None
-        assert bias_v is None
-
+    # TODO: Implement using NestedSize and constructor
+    import pdb; pdb.set_trace()
     q = q.contiguous().view(tgt_len, bsz * num_heads, head_dim).transpose(0, 1)
     if k is not None:
         k = k.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)
     if v is not None:
         v = v.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)
 
-    if static_k is not None:
-        assert static_k.size(0) == bsz * num_heads
-        assert static_k.size(2) == head_dim
-        k = static_k
-
-    if static_v is not None:
-        assert static_v.size(0) == bsz * num_heads
-        assert static_v.size(2) == head_dim
-        v = static_v
-
     src_len = k.size(1)
-
-    if key_padding_mask is not None:
-        assert key_padding_mask.size(0) == bsz
-        assert key_padding_mask.size(1) == src_len
-
-    if add_zero_attn:
-        src_len += 1
-        k = torch.cat([k, torch.zeros((k.size(0), 1) + k.size()[2:], dtype=k.dtype, device=k.device)], dim=1)
-        v = torch.cat([v, torch.zeros((v.size(0), 1) + v.size()[2:], dtype=v.dtype, device=v.device)], dim=1)
-        if attn_mask is not None:
-            attn_mask = pad(attn_mask, (0, 1))
-        if key_padding_mask is not None:
-            key_padding_mask = pad(key_padding_mask, (0, 1))
 
     attn_output_weights = torch.bmm(q, k.transpose(1, 2))
     assert list(attn_output_weights.size()) == [bsz * num_heads, tgt_len, src_len]
-
-    if attn_mask is not None:
-        if attn_mask.dtype == torch.bool:
-            attn_output_weights.masked_fill_(attn_mask, float('-inf'))
-        else:
-            attn_output_weights += attn_mask
-
-    if key_padding_mask is not None:
-        attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
-        attn_output_weights = attn_output_weights.masked_fill(
-            key_padding_mask.unsqueeze(1).unsqueeze(2),
-            float('-inf'),
-        )
-        attn_output_weights = attn_output_weights.view(bsz * num_heads, tgt_len, src_len)
 
     attn_output_weights = F.softmax(
         attn_output_weights, dim=-1)
@@ -362,12 +255,13 @@ def multi_head_attention_forward(query,                           # type: Tensor
     attn_output = attn_output.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
     attn_output = F.linear(attn_output, out_proj_weight, out_proj_bias)
 
-    if need_weights:
-        # average attention weights over heads
-        attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
-        return attn_output, attn_output_weights.sum(dim=1) / num_heads
-    else:
-        return attn_output, None
+    # if need_weights:
+    #     # average attention weights over heads
+    #     attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
+    #     return attn_output, attn_output_weights.sum(dim=1) / num_heads
+    # else:
+    # TODO: All relevant callsites in detr ignore the second value
+    return attn_output, None
 
 
 class MultiheadAttention(Module):
